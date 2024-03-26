65774291c972dbf952b0a556ad4eb0fdd00b731a
Bilal Moussou Bouychfar <92933312+NIU1638880@users.noreply.github.com>
2024-03-26 16:58:29 +0100

    login & register v1

---
+++ source/app/src/main/java/com/uablis/easyfitness/CreateAccountActivity.java
@@ -0,0 +1,17 @@
+package com.uablis.easyfitness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
--- source/app/src/main/java/com/uablis/easyfitness/MainActivity.java
+++ source/app/src/main/java/com/uablis/easyfitness/MainActivity.java
@@ -3,13 +3,51 @@ package com.uablis.easyfitness;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etEmail, etPassword;
+    private Button btnLogin, btnCreateAccount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnCreateAccount = findViewById(R.id.btnCreateAccount);
+
+        // Listener para el botón de Login
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Aquí iría la lógica para el login
+                String email = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+                login(email, password);
+            }
+        });
+
+        // Listener para el botón de Crear Cuenta
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCreateAccountScreen();
+            }
+        });
     }
 
+    private void login(String email, String password) {
 
+    }
+
+    private void openCreateAccountScreen() {
+        Intent intent = new Intent(MainActivity.this, CreateAccountActivity.class);
+        startActivity(intent);
+    }
 }
