47296b61bc27de1c379f2ce9228a3cd99107f033
Bilal Moussou Bouychfar <92933312+NIU1638880@users.noreply.github.com>
2024-04-05 14:50:02 +0200

    api v1

---
+++ Materials/easyfitness/src/main/java/com/uablis/easyfitness/EasyfitnessApplication.java
@@ -0,0 +1,13 @@
+package com.uablis.easyfitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyfitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyfitnessApplication.class, args);
+	}
+
+}
