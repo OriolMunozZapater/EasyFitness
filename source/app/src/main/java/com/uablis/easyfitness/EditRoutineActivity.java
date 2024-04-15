29f58202a7dc7d16c0a9cbdaee7b9efe2f15299a
Aniolb <aniolboladeras@gmail.com>
2024-04-15 11:41:58 +0200

    S'ha afegit funcionalitats al EditRoutineActivity

--- source/app/src/main/java/com/uablis/easyfitness/EditRoutineActivity.java
+++ source/app/src/main/java/com/uablis/easyfitness/EditRoutineActivity.java
@@ -2,10 +2,13 @@ package com.uablis.easyfitness;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.Button;
@@ -31,6 +34,27 @@ public class EditRoutineActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit_routine);
 
         eliminateCross = findViewById(R.id.eliminate_cross);
-
+        eliminateCross.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context;
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("Confirmació")
+                        .setMessage("Estàs segur que vols eliminar?")
+                        .setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Eliminar l'element aquí
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Cancelar l'acció
+                            }
+                        })
+                        .show();
+            }
+        });
     }
 }
