007967d5f404977e916874bf66abbd0b36cf37e5
NIU1636546 <1636546@uab.cat>
2024-04-15 09:38:21 +0200

    API ejercicio

---
+++ Materials/easyfitness/src/main/java/com/uablis/easyfitness/model/Ejercicio.java
@@ -0,0 +1,88 @@
+package com.uablis.easyfitness.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ejercicio")
+public class Ejercicio {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer ejercicioID;
+
+  @Column(nullable = false)
+  private Integer rutinaID;
+
+  @Column(nullable = false)
+  private String tipo; //normal, calentamiento, serie descendente y al fallo.
+
+  @Column(nullable = false)
+  private String descripcion;
+
+  @Column(nullable = false)
+  private String nombre;
+
+  @Column(nullable = false)
+  private Double valoracion;
+
+  @Column(nullable = false)
+  private String grupoMuscular;
+
+  //Setters
+  public void setEjercicioID(Integer ejercicioID) {
+    this.ejercicioID = ejercicioID;
+  }
+
+  public void setRutinaID(Integer rutinaID) {
+    this.rutinaID = rutinaID;
+  }
+
+  public void setTipo(String tipo) {
+    this.tipo = tipo;
+  }
+
+  public void setDescripcion(String descripcion) {
+    this.descripcion = descripcion;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public void setValoracion(Double valoracion) {
+    this.valoracion = valoracion;
+  }
+
+  public void setGrupoMuscular(String grupoMuscular) {
+    this.grupoMuscular = grupoMuscular;
+  }
+
+  // Getters
+  public Integer getEjercicioID() {
+    return ejercicioID;
+  }
+
+  public Integer getRutinaID() {
+    return rutinaID;
+  }
+
+  public String getTipo() {
+    return tipo;
+  }
+
+  public String getDescripcion() {
+    return descripcion;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public Double getValoracion() {
+    return valoracion;
+  }
+
+  public String getGrupoMuscular() {
+    return grupoMuscular;
+  }
+}
