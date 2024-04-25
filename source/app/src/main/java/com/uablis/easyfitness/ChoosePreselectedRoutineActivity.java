48cbe627cc373d4db984fbaf9d86fab034829033
NIU1632753 <1632753@uab.cat>
2024-04-25 18:49:58 +0200

    No he cambiado nada pero me obliga

--- source/app/src/main/java/com/uablis/easyfitness/ChoosePreselectedRoutineActivity.java
+++ source/app/src/main/java/com/uablis/easyfitness/ChoosePreselectedRoutineActivity.java
@@ -11,21 +11,32 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.PopupMenu;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
+import java.io.UnsupportedEncodingException;
+
 public class ChoosePreselectedRoutineActivity extends AppCompatActivity {
-    private TextView hola;
     private ImageView home, training_routines, training, profile, backArrow;
-    private Toolbar toolbar, appbar;
-    private ImageButton menu;
-    private RelativeLayout routine;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,16 +46,9 @@ public class ChoosePreselectedRoutineActivity extends AppCompatActivity {
         home = findViewById(R.id.home);
         training_routines = findViewById(R.id.training_routines);
         training = findViewById(R.id.training_session);
-        menu = findViewById(R.id.menu_button);
         backArrow = findViewById(R.id.back_arrow);
-        routine = findViewById(R.id.preSelectedRoutine);
+        String[] routineNames = {"superaniol", "aniolpeirna"}; String[] routineID = {"1", "2"};
 
-        menu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                menuPopUpRoutine(v);
-            }
-        });
 
         backArrow.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,17 +57,185 @@ public class ChoosePreselectedRoutineActivity extends AppCompatActivity {
             }
         });
 
-        routine.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addRoutine();
-            }
-        });
+        updateUIWithRoutines(routineNames, routineID, "1234");
     }
 
-    public void addRoutine() {
-        //logica per afegir rutina com a seleccionada i mostrarla a view_trainnig_routine
-        finish();
+    private void loadPredifinedRoutine() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String userId = UsuarioActual.getInstance().getUserId();
+        String url = "http://192.168.1.97:8080/api/rutinas/usuario/" + 0;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray jsonArray = new JSONArray(response);
+                            String[] routineIDs = new String[jsonArray.length()];
+                            String[] routineNames = new String[jsonArray.length()];
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                routineIDs[i] = jsonObject.getString("rutinaID");
+                                routineNames[i] = jsonObject.getString("nombre");
+                            }
+                            updateUIWithRoutines(routineNames, routineIDs, userId);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(ChoosePreselectedRoutineActivity.this, "Error parsing JSON data", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                Toast.makeText(ChoosePreselectedRoutineActivity.this, "Error making API call: " + error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        queue.add(stringRequest);
+    }
+    private void updateUIWithRoutines(String[] routineNames, String[] routineIDs, String userID) {
+        LinearLayout routinesLayout = findViewById(R.id.preRoutineContainer);
+
+        for (String name : routineNames) {
+            View routineView = getLayoutInflater().inflate(R.layout.routine_item, routinesLayout, false);
+            TextView textView = routineView.findViewById(R.id.textViewRoutineName);
+            ImageButton menuButton = routineView.findViewById(R.id.menu_button_routine);
+
+            textView.setText(name);
+            //eto ns
+            int finalI = 0;
+            int finalI1 = finalI;
+            routineView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    copyRoutine(routineIDs[finalI1], userID);
+                }
+            });
+            menuButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    menuPopUpRoutine(v);
+                }
+            });
+            routinesLayout.addView(routineView);
+
+            finalI++;
+        }
+    }
+
+    protected void copyRoutine(String copiedRoutineID, String userID) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://192.168.1.97:8080/api/rutinas/" + copiedRoutineID;
+
+        // Primera solicitud GET para obtener los detalles de la rutina
+        StringRequest getRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray jsonArray = new JSONArray(response);
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                String routineName = jsonObject.getString("nombre");
+                                String routineDescription = jsonObject.getString("descripcion");
+                                String publicRoutine = jsonObject.getString("publico");
+                                insertNewRoutine(routineName, routineDescription, publicRoutine, userID);
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(ChoosePreselectedRoutineActivity.this, "Error parsing JSON data", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    private void insertNewRoutine(String routineName, String routineDescription, String publicRoutine, String userID) {
+                        // Construye el cuerpo de la solicitud en formato JSON
+                        JSONObject jsonBody = new JSONObject();
+                        try {
+                            jsonBody.put("nombre", routineName);
+                            jsonBody.put("descripcion", routineDescription);
+                            jsonBody.put("user_ID", userID);
+                            jsonBody.put("publico", publicRoutine);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        // URL para la solicitud POST
+                        String postUrl = "http://192.168.1.97:8080/api/rutinas/";
+
+                        // Crea una solicitud POST
+                        JsonObjectRequest postRequest = new JsonObjectRequest(Request.Method.POST, postUrl, jsonBody,
+                                new Response.Listener<JSONObject>() {
+                                    @Override
+                                    public void onResponse(JSONObject response) {
+                                        // La inserción fue exitosa
+                                        showAlert("Éxito", "Rutina creada exitosamente y datos guardados en la API.");
+                                        String idNewRoutine = getNewRoutineID(routineName, userID);
+                                        String postUrl = "http://192.168.1.97:8080/api/rutinas/";
+                                    }
+
+                                    private String getNewRoutineID(String routineName, String userID) {
+
+                                        return "";
+                                    }
+                                },
+                                new Response.ErrorListener() {
+                                    @Override
+                                    public void onErrorResponse(VolleyError error) {
+                                        // Imprime detalles del error
+                                        error.printStackTrace();
+                                        if (error.networkResponse != null) {
+                                            String body;
+                                            //Obtiene el cuerpo de la respuesta
+                                            final String statusCode = String.valueOf(error.networkResponse.statusCode);
+                                            try {
+                                                body = new String(error.networkResponse.data, "UTF-8");
+                                                showAlert("Error de la API", "Error al guardar los datos de la rutina en la API. Código de estado: " + statusCode + " Cuerpo: " + body);
+                                            } catch (UnsupportedEncodingException e) {
+                                                e.printStackTrace();
+                                            }
+                                        } else {
+                                            showAlert("Error de la API", "Error al guardar los datos de la rutina en la API. Error: " + error.toString());
+                                        }
+                                    }
+                                }
+                        );
+
+                        // Añade la solicitud a la cola
+                        queue.add(postRequest);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                Toast.makeText(ChoosePreselectedRoutineActivity.this, "Error making API call: " + error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        queue.add(getRequest);
+    }
+
+    private void showAlert(String title, String message) {
+        androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(ChoosePreselectedRoutineActivity.this, R.style.AlertDialogTheme);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // Redirige al usuario a la pantalla de inicio de sesión
+                        navigateToLogin();
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+    private void navigateToLogin() {
+        Intent intent = new Intent(ChoosePreselectedRoutineActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // Limpia el back stack
+        startActivity(intent);
     }
 
     public void backScreen() {
@@ -115,8 +287,4 @@ public class ChoosePreselectedRoutineActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void ScreenMain() {
-        Intent intent = new Intent(ChoosePreselectedRoutineActivity.this, LoginActivity.class);
-        startActivity(intent);
-    }
 }
---
+++ source/app/src/main/java/com/uablis/easyfitness/ProfileActivity.java
@@ -0,0 +1,90 @@
+package com.uablis.easyfitness;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private ImageView home, training_routines, training, profile, profile_image;
+
+    private Toolbar toolbar, appbar;
+
+    private ImageButton edit_profile;
+
+    private TextView pesObjectiu, pesPropi, nomUser, sexe, altura;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        //recuperarDatosPerfil();
+
+        profile = findViewById(R.id.profile);
+        home = findViewById(R.id.home);
+        training_routines = findViewById(R.id.training_routines);
+        training = findViewById(R.id.training_session);
+
+        edit_profile = findViewById(R.id.btnEditPersonalData);
+
+        edit_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToEditData();
+            }
+        });
+
+        training_routines.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, TrainingRoutinesActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    public void recuperarDatosPerfil() {
+        // Obtener referencias a los elementos de la interfaz
+        TextView pesoObjetivoTextView = findViewById(R.id.pesObjectiu);
+        TextView pesoActualTextView = findViewById(R.id.pesPropi);
+        TextView nombreTextView = findViewById(R.id.nomUser);
+        TextView alturaTextView = findViewById(R.id.altura);
+        TextView sexoTextView = findViewById(R.id.sexe);
+
+        // Recuperar datos de BD
+
+        // Peso objetivo
+        /*int pesoObjetivo = preferencias.getInt("peso_objetivo", 0);
+        pesoObjetivoTextView.setText(String.valueOf(pesoObjetivo) + " Kg");
+
+        // Peso actual
+        int pesoActual = preferencias.getInt("peso_actual", 0);
+        pesoActualTextView.setText(String.valueOf(pesoActual) + " Kg");
+
+        // Nombre
+        String nombre = preferencias.getString("nombre", "");
+        nombreTextView.setText(nombre);
+
+        // Altura
+        int altura = preferencias.getInt("altura", 0);
+        alturaTextView.setText(String.valueOf(altura) + " cm");
+
+        // Sexo
+        String sexo = preferencias.getString("sexo", "");
+        sexoTextView.setText(sexo);*/
+    }
+    public void goToEditData() {
+        Intent intent = new Intent(ProfileActivity.this, /*CAMBIAR LO SIGUIENTE POR EDITAR_DETALLS_PERFIL*/EditRoutineActivity.class);
+        startActivity(intent);
+    }
+
+}
+
