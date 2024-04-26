da34e636b0a0bb9c9c23ba26fd0bed8bc790c0cc
Bilal Moussou Bouychfar <92933312+NIU1638880@users.noreply.github.com>
2024-04-26 02:37:11 +0200

    tabla registro

---
+++ Materials/easyfitness/src/main/java/com/uablis/easyfitness/model/Registro.java
@@ -0,0 +1,52 @@
+package com.uablis.easyfitness.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "registro")
+public class Registro {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer registroID;
+
+  private String nombreRutina;
+
+  private java.sql.Time tiempoTardado;
+
+  // Setters
+  public void setRegistroID(Integer registroID) {
+    this.registroID = registroID;
+  }
+
+  public void setNombreRutina(String nombreRutina) {
+    this.nombreRutina = nombreRutina;
+  }
+
+  public void setTiempoTardado(java.sql.Time tiempoTardado) {
+    this.tiempoTardado = tiempoTardado;
+  }
+
+  // Getters
+  public Integer getRegistroID() {
+    return registroID;
+  }
+
+  public String getNombreRutina() {
+    return nombreRutina;
+  }
+
+  public java.sql.Time getTiempoTardado() {
+    return tiempoTardado;
+  }
+
+  @Column(name = "userID")
+  private Integer userID;
+
+  public void setUserID(Integer userID) {
+    this.userID = userID;
+  }
+
+  public Integer getUserID() {
+    return userID;
+  }
+}
