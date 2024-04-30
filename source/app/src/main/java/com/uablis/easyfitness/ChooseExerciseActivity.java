809d9a798ad4ccc410878e86cd9e5860b22d6bc9
NIU1636054 <92090779+NIU1636054@users.noreply.github.com>
2024-04-30 13:04:53 +0200

    chooseEc¡xeerciseActivity v1

--- source/app/src/main/java/com/uablis/easyfitness/ChooseExerciseActivity.java
+++ source/app/src/main/java/com/uablis/easyfitness/ChooseExerciseActivity.java
@@ -5,21 +5,36 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.SearchView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.search.SearchBar;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ChooseExerciseActivity extends AppCompatActivity {
     private SearchBar exerciseSearch;
+    LinearLayout exerciseContainer;
     private Button selectMuscle, newExercise;
     private ImageView backArrow;
 
@@ -33,6 +48,10 @@ public class ChooseExerciseActivity extends AppCompatActivity {
         selectMuscle = findViewById(R.id.selectMuscle);
         backArrow = findViewById(R.id.back_arrow);
         newExercise = findViewById(R.id.btnNewExercise);
+
+        int userID = Integer.parseInt(UsuarioActual.getInstance().getUserId());
+        loadEjercicios(0);
+        loadEjercicios(userID);
 
         newExercise.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,6 +72,59 @@ public class ChooseExerciseActivity extends AppCompatActivity {
                 selectMuscle();
             }
         });
+    }
+
+    private void loadEjercicios(int userID) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://192.168.0.19:8080/api/ejercicios/user/" + userID;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray jsonArray = new JSONArray(response);
+                            String[] exerciseNames = new String[jsonArray.length()];
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                exerciseNames[i] = jsonObject.getString("nombre");
+                            }
+
+                            updateUIWithExercices(exerciseNames);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(ChooseExerciseActivity.this, "Error parsing JSON data", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                Toast.makeText(ChooseExerciseActivity.this, "Error making API call: " + error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        queue.add(stringRequest);
+    }
+
+    private void updateUIWithExercices(String[] exerciseNames) {
+        LinearLayout routinesLayout = findViewById(R.id.exerciseContainer);
+
+        for (String name : exerciseNames) {
+            View exerciseView = getLayoutInflater().inflate(R.layout.exercise_row2, routinesLayout, false);
+            TextView textView = exerciseView.findViewById(R.id.tvExerciseName);
+
+
+            textView.setText(name);
+            exerciseView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+            routinesLayout.addView(exerciseView);
+        }
     }
 
     public void goToNewExercise() {
