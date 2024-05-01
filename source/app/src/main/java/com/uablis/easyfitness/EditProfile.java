fd0dd1e66537bd361af619008fffc9b4f67dccc3
NIU1632753 <1632753@uab.cat>
2024-05-01 18:57:50 +0200

    Faltaban un par de cosas en editar perfil

--- source/app/src/main/java/com/uablis/easyfitness/EditProfile.java
+++ source/app/src/main/java/com/uablis/easyfitness/EditProfile.java
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
 package com.uablis.easyfitness;
+
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
@@ -25,6 +25,8 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.uablis.easyfitness.R;
+import com.uablis.easyfitness.UsuarioActual;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -80,7 +82,7 @@ public class EditProfile extends AppCompatActivity {
                         nameUser.toString(), btnSexSelect.getText().toString(), cognomUser.toString(), descripcioUser.toString(), DateOfBirthUser.toString());
             }
         });
-
+         getUserData();
 
     }
 
@@ -227,119 +229,3 @@ public class EditProfile extends AppCompatActivity {
     }
 }
 
-=======
-package com.uablis.easyfitness;
-
-import android.app.DatePickerDialog;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
-import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.Calendar;
-
-public class EditProfile extends AppCompatActivity {
-    private static final int PICK_IMAGE_REQUEST = 1;
-
-    private CharSequence[] genderOptions = {"Masculino", "Femenino", "Otros", "Prefiero no decirlo", "Croissant"};
-
-    private EditText nameUser, cognomUser, pesActualUser, alturaUser, descripcioUser;
-    private TextView DateOfBirthUser;
-    private ImageView imagePerfilUser;
-    private Button btnSexSelect, btnSaveUserChanges;
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_profile);
-
-        // Initialize views
-        nameUser = findViewById(R.id.nameUser);
-        cognomUser = findViewById(R.id.cognomUser);
-        pesActualUser = findViewById(R.id.pesActualUser);
-        alturaUser = findViewById(R.id.alturaUser);
-        descripcioUser = findViewById(R.id.descripcioUser);
-        DateOfBirthUser = findViewById(R.id.DateOfBirthUser);
-        imagePerfilUser = findViewById(R.id.imagePerfilUser);
-        btnSexSelect = findViewById(R.id.btnSexSelect);
-        btnSaveUserChanges = findViewById(R.id.btnSaveUserChanges);
-
-        // Set click listener for image selection
-        imagePerfilUser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                selectImageFromGallery();
-            }
-        });
-
-        // Set click listener for gender selection
-        btnSexSelect.setOnClickListener(this::showSexoOptions);
-
-        DateOfBirthUser.setOnClickListener(v -> showDatePickerDialog());
-
-        // Set click listener for save button
-        btnSaveUserChanges.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Implement save functionality here
-                saveUserChanges();
-            }
-        });
-    }
-
-    private void showDatePickerDialog() {
-        final Calendar calendar = Calendar.getInstance();
-        DatePickerDialog datePickerDialog = new DatePickerDialog(
-                EditProfile.this,
-                (view, year, month, dayOfMonth) -> DateOfBirthUser.setText(String.format("%d-%02d-%02d", year, month + 1, dayOfMonth)),
-                calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)
-        );
-        datePickerDialog.show();
-    }
-
-    public void showSexoOptions(View view) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Selecciona tu sexo");
-        builder.setItems(genderOptions, (dialog, which) -> btnSexSelect.setText(genderOptions[which]));
-        builder.show();
-    }
-
-    private void selectImageFromGallery() {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_PICK);
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            Uri uri = data.getData();
-            try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-                // Aquí puedes setear el bitmap en tu ImageView
-                imagePerfilUser.setImageBitmap(bitmap);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private void saveUserChanges() {
-        //TODO: implementar logica de guardar cambios
-    }
-}
-
->>>>>>> a897230213c6b8d9bf5816a808364420c2e13928
